4c8d9069b71114cb8480f88840f7e9be0eb00002
Matheus <matheusvictor1292@gmail.com>
2023-11-27 21:24:31 -0300

    Tudo que eu sei sobre isso

---
+++ dandd/Models/Race.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dandd.Models
+{
+    public class Race
+    {
+        public string Name { get; set; }
+        public int RaceId { get; set; }
+    }
+}
--- dandd/Views/RaceView.xaml.cs
+++ dandd/Views/RaceView.xaml.cs
@@ -1,3 +1,5 @@
+using dandd.Models;
+
 namespace dandd.Views;
 
 public partial class RaceView : ContentPage
@@ -5,5 +7,11 @@ public partial class RaceView : ContentPage
 	public RaceView()
 	{
 		InitializeComponent();
+		var race = new Race
+		{
+			RaceId = 1,	
+			Name = "Dragonborn"
+		};
+		BindingContext= race;
 	}
 }
